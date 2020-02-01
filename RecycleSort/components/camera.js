d0fa1782ec5cbf63892a15c53d831929466eac73
Jaden Lee <jadenjlee@outlook.com>
2020-02-01 17:16:48 -0500

    Set up basic folder for camera and labeler.

---
+++ RecycleSort/components/camera.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class Camera extends Component {
+    'TODO: Create Camera component with button to take picture'
+  render(
+  )
+}
