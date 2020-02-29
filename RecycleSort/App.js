0286866c783a745ffa0144538a5bebafb8c296a3
Jaden Lee <jadenjlee@outlook.com>
2020-02-29 13:35:41 -0500

    Camera app - without storing functionality

--- RecycleSort/App.js
+++ RecycleSort/App.js
@@ -1,19 +1,8 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import Camera_Page from './components/camera';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return <Camera_Page />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- RecycleSort/components/labeler_button.js
+++ RecycleSort/components/labeler_button.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class Labeler extends Component {
-    'TODO: Create labeler component with button to retrieve pictures from local storage and label'
-  render(
-  )
+  'TODO: Create labeler component with button to retrieve pictures from local storage and label';
+  render() {
+    return <div> sice </div>;
+  }
 }
